fa77345ec5cf9d0cd0568570d8371e030e8d4f6d
Merlin Thomas <merlinthomas0817@gmail.com>
2018-05-21 19:56:45 +0530

    Comments and CSS

--- src/gitfindfriend/component/find_git_friend_example.js
+++ src/gitfindfriend/component/find_git_friend_example.js
@@ -2,86 +2,84 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import '../css/index.css';
 
+/* Function Component */
 const FriendsDetails = (props) => {
     return (
+        /* Providing inline CSS property */
         <div style={{ margin: '1em', textAlign: 'center' }}>
-            {/* using avatar_url = "https://avatars0.githubusercontent.com/u/15608874?v=4" 
-    from https://api.github.com/users/merlin8thomas*/}
-            <img width='75' src={props.avatar_url} />
-            <div style={{ display: 'inline-block', marginLeft: 10 }}>
-                <div style={{ fontSize: '1.25em', fontWeight: 'bold' }}>
+            <img width='125' src={props.avatar_url} />
+            <div style={{ display: 'inline-block', marginLeft: 20 }}>
+                <div style={{ fontSize: '2em', fontWeight: 'bold', color: 'blue' }}>
                     {props.name}
                 </div>
-                <div>{props.company}</div>
+                <div style={{ fontWeight: 'bold' }}>{props.company}</div>
+
+                <div style={{ fontWeight: 'bold' }}>{props.location}</div>
             </div>
         </div>
     )
 }
 
-/* let friend = [
-    {
-        name: "Merlin Thomas",
-        avatar_url: "https://avatars0.githubusercontent.com/u/15608874?v=4",
-        company: "Synechron"
-    }
-] */
-
+/* Function Component */
 const GitFriendsList = (props) => {
 
     return (
         <div>
             {
-                props.friends.map(friend => <FriendsDetails {...friend} />)}
+                props.friends.map(friend => <FriendsDetails key={friend.id} {...friend} />)}
         </div>
     )
 }
 
-class SearchFriend extends Component{
-    state = {userName : ''};
+/* Class Component */
+class SearchFriend extends Component {
+    state = { userName: '' };
     handleSubmit = (event) => {
         event.preventDefault();
+        /* Axios is a promised-based library that's similar to the Fetch API. It helps in making XMLHttpRequests from the browser */
         axios.get(`https://api.github.com/users/${this.state.userName}`)
-        .then(resp => {
-            this.props.onSubmit(resp.data);
-            this.setState({userName : ''});
-        })
+            .then(resp => {
+                this.props.onSubmit(resp.data);
+                this.setState({ userName: '' });
+            })
     }
-    render(){
-        return(
+    render() {
+        return (
             <form onSubmit={this.handleSubmit}>
-            <input type='text' 
-            className = "search"
-            value={this.state.userName}
-            onChange={(event) => this.setState({userName: event.target.value})}
-            placeholder="Github Username Required"
-            />
-            <button className='button' type="submit">Search Friend</button>
+                <input type='text'
+                    className="search"
+                    value={this.state.userName}
+                    onChange={(event) => this.setState({ userName: event.target.value })}
+                    placeholder="Github Username Required"
+                />
+                <button className='button' type="submit">Search Friend</button>
             </form>
         )
     }
 }
 
-class SearchResult extends Component{
+/* Class Component */
+class SearchResult extends Component {
     state = {
-        friends : []
+        friends: []
     }
 
     searchGitFriend = (searchInfo) => {
         this.setState(prevState => ({
-            friends : prevState.friends.concat(searchInfo)
+            friends: prevState.friends.concat(searchInfo)
         }))
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div
-            className='background'
-            style={{margin:'auto',width:'50%',border:'3px blue', padding:'10px', textAlign:'center'}}>
-            <h1>Search A Git Friend</h1>
+                className='background'
+                style={{ margin: 'auto', width: '50%', border: '3px blue', padding: '10px', textAlign: 'center' }}>
+                <h1>Search A Git Friend</h1>
                 <SearchFriend onSubmit={this.searchGitFriend} />
                 &nbsp; &nbsp; &nbsp;
-                <GitFriendsList friends={this.state.friends}/>
-                </div>
+                <GitFriendsList friends={this.state.friends} />
+            </div>
         )
     }
 }
